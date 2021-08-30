5291685d46bbfbfdda8f64537524bc70bed75794
Igor Lisitsyn <bit1@list.ru>
2021-08-30 15:52:41 +1200

    формирование псевдо графического вывода

--- src/main/kotlin/Hangman/Hang.kt
+++ src/main/kotlin/Hangman/Hang.kt
@@ -1,6 +1,7 @@
 package Hangman
 
 import kotlin.random.Random
+import kotlin.random.nextInt
 
 val words = listOf<String>("Хроника","Кафель","Камбала","Лаваш","Бальзам","Бревно")
 var word = ""
@@ -10,11 +11,89 @@ val mistakes = 0
 
 fun main(args: Array<String>) {
     setupGame()
-    println(word)
+    val value = Random.nextInt(0..6)
+    printMistakeStatus(value)
 
 }
 
 fun setupGame() {
     val wordIndex = Random.nextInt(words.size)
     word = words[wordIndex].toUpperCase()
+}
+
+fun printMistakeStatus(mistake: Int){
+    when(mistake) {
+        0 -> print0Mistake()
+        1 -> print1Mistake()
+        2 -> print2Mistake()
+        3 -> print3Mistake()
+        4 -> print4Mistake()
+        5 -> print5Mistake()
+        6 -> print6Mistake()
+     }
+}
+
+
+fun print0Mistake(){
+    println("  |------|")
+    println("  |      |")
+    println("  |       ")
+    println("  |       ")
+    println("  |       ")
+    println(" /|\\     ")
+    println("/ | \\    ")
+}
+fun print1Mistake(){
+    println("  |------|")
+    println("  |      |")
+    println("  |      o")
+    println("  |       ")
+    println("  |       ")
+    println(" /|\\     ")
+    println("/ | \\    ")
+}
+fun print2Mistake(){
+    println("  |------|")
+    println("  |      |")
+    println("  |      o")
+    println("  |      |")
+    println("  |      |")
+    println(" /|\\     ")
+    println("/ | \\    ")
+}
+fun print3Mistake(){
+    println("  |------|")
+    println("  |      |")
+    println("  |      o")
+    println("  |     /|")
+    println("  |      |")
+    println(" /|\\     ")
+    println("/ | \\    ")
+}
+fun print4Mistake(){
+    println("  |------|")
+    println("  |      |")
+    println("  |      o")
+    println("  |     /|\\")
+    println("  |      |")
+    println(" /|\\     ")
+    println("/ | \\    ")
+}
+fun print5Mistake(){
+    println("  |------|")
+    println("  |      |")
+    println("  |      o")
+    println("  |     /|\\")
+    println("  |      |")
+    println(" /|\\   / ")
+    println("/ | \\    ")
+}
+fun print6Mistake(){
+    println("  |------|")
+    println("  |      |")
+    println("  |      o")
+    println("  |     /|\\")
+    println("  |      |")
+    println(" /|\\    / \\")
+    println("/ | \\    ")
 }
